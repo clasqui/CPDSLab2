6f4f3fa003d29e1f882cb0869f8fc4ac6db7411a
Jaimi5 <janms2009@gmail.com>
2022-10-23 19:54:07 +0200

    Start remote function in opty module

--- opty-source-files/opty.erl
+++ opty-source-files/opty.erl
@@ -1,5 +1,5 @@
 -module(opty).
--export([start/5, stop/1]).
+-export([start/6, stop/2, startRemote/6]).
 
 %% Clients: Number of concurrent clients in the system
 %% Entries: Number of entries in the store
@@ -7,39 +7,38 @@
 %% Writes: Number of write operations per transaction
 %% Time: Duration of the experiment (in secs)
 
-start(Clients, Entries, Reads, Writes, Time) ->
+start(Clients, Entries, Reads, Writes, Time, RemoteIns) ->
     register(s, server:start(Entries)),
-    L = startClients(Clients, [], Entries, Reads, Writes),
-    io:format(
-        "Starting: ~w CLIENTS, ~w ENTRIES, ~w RDxTR, ~w WRxTR, DURATION ~w s~n",
-        [Clients, Entries, Reads, Writes, Time]
-    ),
-    timer:sleep(Time * 1000),
-    stop(L).
+    L = startClients(Clients, [], Entries, Reads, Writes, s),
+    io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w RDxTR, ~w WRxTR, DURATION ~w s~n",
+         [Clients, Entries, Reads, Writes, Time]),
+    timer:sleep(Time*1000),
+    stop(L, RemoteIns).
 
-stop(L) ->
+stop(L, RemoteIns) ->
     io:format("Stopping...~n"),
     stopClients(L),
-    waitClients(L),
-    s ! stop,
+    s ! RemoteIns,
     io:format("Stopped~n").
 
-startClients(0, L, _, _, _) ->
-    L;
-startClients(Clients, L, Entries, Reads, Writes) ->
-    Pid = client:start(Clients, Entries, Reads, Writes, s),
-    startClients(Clients - 1, [Pid | L], Entries, Reads, Writes).
+startRemote(Clients, Entries, Reads, Writes, Time, RemoteIns) ->
+  L = startClients(Clients, [], Entries, Reads, Writes, RemoteIns),
+  io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w RDxTR, ~w WRxTR, DURATION ~w s~n",
+       [Clients, Entries, Reads, Writes, Time]),
+  timer:sleep(Time*1000),
+  stopClients(L).
+
+startClients(0, L, _, _, _, _) -> L;
+startClients(Clients, L, Entries, Reads, Writes, RemoteIns) ->
+    Pid = client:start(Clients, Entries, Reads, Writes, RemoteIns),
+    startClients(Clients-1, [Pid|L], Entries, Reads, Writes, RemoteIns).
 
 stopClients([]) ->
     ok;
-stopClients([Pid | L]) ->
+stopClients([Pid|L]) ->
     Pid ! {stop, self()},
+    receive
+        {done, Pid} -> ok
+    end,
     stopClients(L).
 
-waitClients([]) ->
-    ok;
-waitClients(L) ->
-    receive
-        {done, Pid} ->
-            waitClients(lists:delete(Pid, L))
-    end.
