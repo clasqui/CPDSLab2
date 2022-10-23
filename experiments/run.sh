623bfc9395f916a21777ba3bfb0a05148754fd56
Marc Clascà <marc.clasca@bsc.es>
2022-10-23 20:23:47 +0200

    Adds script to run experiments

---
+++ experiments/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ../opty-source-files/
+echo "Studying parameters in ${1}"
+while read CONF; do
+	echo "Running configuration $CONF" >&2
+
+	erl +S 2:2 -noshell -eval "opty:start($CONF)" -run init stop < /dev/null
+done < ../experiments/${1}
