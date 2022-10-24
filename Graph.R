0a80b235d18d80839dafb907f2ce0c48c922d647
Jaimi5 <janms2009@gmail.com>
2022-10-24 18:13:45 +0200

    Script R updated for Read/Write ratio

--- Graph.R
+++ Graph.R
@@ -4,7 +4,7 @@ nReadPerf <- read.csv('./experiments/rops-results.csv', header=FALSE, colClasses
 nWritesPerf <- read.csv('./experiments/wops-results.csv', header=FALSE, colClasses=c('numeric', rep('numeric', 3), 'numeric'))
 nReadWritePerf <- read.csv('./experiments/rwratio-results.csv', header=FALSE, colClasses=c('numeric', rep('numeric', 3), 'numeric'))
 
-nReadWritePerf$V6<-nReadWritePerf$V3/nReadWritePerf$V4
+nReadWritePerf$V6<-nReadWritePerf$V3/(nReadWritePerf$V3 + nReadWritePerf$V4)
 
 par(mfrow=c(1,1))
 
@@ -12,18 +12,19 @@ plot(nClientsPerf$V5 ~ nClientsPerf$V1, data=nClientsPerf, type="b", col="red", 
 plot(nEntriesPerf$V5 ~ nEntriesPerf$V2, data=nEntriesPerf, type="b", col="red", ylab="Successfull transaction rate %", xlab="Number of Entries", main="Number of Entries Increase")
 plot(nReadPerf$V5 ~ nReadPerf$V3, data=nReadPerf, type="b", col="red", ylab="Successfull transaction rate %", xlab="Number of Reads", main="Number of Reads Increase")
 plot(nWritesPerf$V5 ~ nWritesPerf$V4, data=nWritesPerf, type="b", col="red", ylab="Successfull transaction rate %", xlab="Number of Writes", main="Number of Writes Increase")
-plot(nReadWritePerf$V5 ~ nReadWritePerf$V6, data=nReadWritePerf, type="b", col="red", ylab="Successfull transaction rate %", xlab="Ratio of Read/Writes", main="Ratio of Read/Writes Increase")
 
+plot(nReadWritePerf$V5 ~ nReadWritePerf$V6, data=nReadWritePerf, type="b", col="red", ylab="Successfull transaction rate %", xlab="Ratio of Read/Writes", main="Ratio of Read/Writes Increase")
+text(30, 100, expression(infinity), cex=2)
+text(28, 100, "infinity", cex=1) 
 
 ##Experiment with double
 nClients <- read.csv('./timey-source-files/params-nclients.csv', header=FALSE, colClasses=c('numeric', rep('numeric', 3), 'numeric'))
 nEntries <- read.csv('./timey-source-files/params-nentries.csv', header=FALSE, colClasses=c('numeric', rep('numeric', 3), 'numeric'))
 nRead <- read.csv('./timey-source-files/params-rops.csv', header=FALSE, colClasses=c('numeric', rep('numeric', 3), 'numeric'))
 nWrites <- read.csv('./timey-source-files/params-wops.csv', header=FALSE, colClasses=c('numeric', rep('numeric', 3), 'numeric'))
+nReadWrite <- read.csv('./timey-source-files/params-rwratio.csv', header=FALSE, colClasses=c('numeric', rep('numeric', 3), 'numeric'))
 
-nWrites$V6<-nWrites$V3/nWrites$V4
-
-nReadWrite <- read.csv('./experiments/params-results.csv', header=FALSE, colClasses=c('numeric', rep('numeric', 3), 'numeric'))
+nReadWrite$V6<-nReadWrite$V3/(nReadWrite$V3 + nReadWrite$V4)
 
 summary<-summaryBy(nClients ~ V1, data = nClients, 
           FUN = list(mean, max, min, median, sd))
@@ -34,7 +35,7 @@ attach(mtcars)
 par(mfrow=c(2,2))
 
 plot(nClients$V5 ~ nClients$V1, data=nClients, type="b", col="red", ylab="Successfull transaction rate %", xlab="Number of Clients", main="Number of Clients Increase")
-lines(nClientsPerf$V5 ~ nClientsPerf$V1, , type="b", col="blue")
+lines(nClientsPerf$V5 ~ nClientsPerf$V1, type="b", col="blue")
 legend("topright", legend=c("Concurrency Control", "Base"),
        col=c("red", "blue"), lty = 1:1, cex=0.8)
 
@@ -55,7 +56,7 @@ lines(nWritesPerf$V5 ~ nWritesPerf$V4, type="b", col="blue")
 legend("topright", legend=c("Concurrency Control", "Base"),
        col=c("red", "blue"), lty = 1:1, cex=0.8)
 
-plot(nWrites$V5 ~ nWrites$V6, data=nWrites, type="b", col="red", ylab="Successfull transaction rate %", xlab="Ratio of Read/Writes", main="Chaning the ratio of Read/Writes")
+plot(nReadWrite$V5 ~ nReadWrite$V6, data=nReadWrite, type="b", col="red", ylab="Successful transaction rate %", xlab="Ratio of Read/Writes", main="Chaning the ratio of Read/Writes")
 lines(nWritesPerf$V5 ~ nWritesPerf$V6, data=nWritesPerf, type="b", col="blue")
 legend("right", legend=c("Concurrency Control", "Base"),
        col=c("red", "blue"), lty = 1:1, cex=0.8)
